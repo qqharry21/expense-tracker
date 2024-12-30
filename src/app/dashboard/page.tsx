0f738d21fd88e154399471cc9eb79ab66d0283a9
qqharry21 <qqharry21@gmail.com>
2024-12-30 17:31:26 +0800

    fix: remove used code

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -4,7 +4,7 @@ import { ExpenseCategoryPieChart } from '@/components/charts/ExpenseCategoryPieC
 import { MonthlyIncomeExpensesChart } from '@/components/charts/MonthlyIncomeExpensesChart';
 import { NetWorthAreaChart } from '@/components/charts/NetWorthAreaChart';
 import { prisma } from '@/lib/prisma';
-import { endOfMonth, format, startOfMonth, subMonths } from 'date-fns';
+import { endOfMonth, startOfMonth, subMonths } from 'date-fns';
 import { redirect } from 'next/navigation';
 export default async function Page() {
   const session = await auth();
@@ -34,43 +34,7 @@ export default async function Page() {
       },
     })
   ).reverse();
-  console.log('🚨 - sixMonthExpenses', expenses);
 
-  // 準備月份標籤
-  const months = Array.from({ length: 6 }, (_, index) => {
-    const date = subMonths(currentDate, 5 - index);
-    return format(date, 'yyyy-MM');
-  });
-
-  // 初始化每月支出
-  const monthlyExpensesData = months.map((month) => ({
-    month,
-    amount: 0,
-  }));
-
-  // 匯總每月支出
-  expenses.forEach((expense) => {
-    const month = format(new Date(expense.startTime), 'yyyy-MM');
-    const monthData = monthlyExpensesData.find((m) => m.month === month);
-    if (monthData) {
-      monthData.amount += expense.amount;
-    }
-  });
-  console.log('🚨 - monthlyExpensesData', monthlyExpensesData);
-
-  // 準備折線圖數據（支出趨勢）
-  const expenseTrends = monthlyExpensesData;
-
-  // 準備支出類別分佈
-  const categoryMap: { [key: string]: number } = {};
-  expenses.forEach((expense) => {
-    categoryMap[expense.category] =
-      (categoryMap[expense.category] || 0) + expense.amount;
-  });
-  const categoriesData = Object.keys(categoryMap).map((category) => ({
-    name: category,
-    value: categoryMap[category],
-  }));
   return (
     <div className="grid flex-1 gap-10 md:grid-cols-2 md:gap-6 xl:gap-10">
       <MonthlyIncomeExpensesChart />
